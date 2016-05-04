48ef8d2822a397d7d7206b620a7ecdde7f8a8d93
Ash Wilson <ash.d.wilson@gmail.com>
2016-05-03 22:53:19 -0700

    fixing dir creation

--- sitch/config_helper.py
+++ sitch/config_helper.py
@@ -15,7 +15,7 @@ class ConfigHelper:
         self.vault_token = ConfigHelper.get_from_env("VAULT_TOKEN")
         self.vault_url = ConfigHelper.get_from_env("VAULT_URL")
         self.logstash_cert_path = "/run/dbus/crypto/logstash.crt"
-        self.ls_cert = self.set_secret_from_vault()
+        self.ls_cert = self.get_secret_from_vault(self)
         return
 
     def build_logrotate_config(self):
@@ -79,7 +79,7 @@ class ConfigHelper:
                 device_id = x
         return device_id
 
-    def set_secret_from_vault(self):
+    def get_secret_from_vault(self):
         client = hvac.Client(url=self.vault_url, token=self.vault_token)
         print "Get secrets from %s, with path %s" % (self.vault_url,
                                                      self.vault_ls_cert_path)
