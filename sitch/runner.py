c3cb2f6f91fcccb1f8a4246171aad4ad8b944762
Ash Wilson <ash.d.wilson@gmail.com>
2016-05-06 00:38:19 -0700

    getting tired.  bedtime

--- sitch/runner.py
+++ sitch/runner.py
@@ -112,14 +112,14 @@ def sim808_consumer(config):
                     retval["scan_finish"] = utility.get_now_string()
                     retval["scan_location"] = str(config.device_id)
                     retval["scan_program"] = "SIM808"
-                    scan_results_queue.extend(retval)
+                    scan_results_queue.append(retval)
                 elif "lon" in report[0]:
                     retval = dict(scan_job_template)
                     retval["scan_results"] = report
                     retval["scan_finish"] = utility.get_now_string()
                     retval["scan_location"] = str(config.device_id)
                     retval["scan_program"] = "GPS"
-                    scan_results_queue.extend(retval)
+                    scan_results_queue.append(retval)
                 else:
                     print "No match!"
                     retval = dict(scan_job_template)
@@ -164,10 +164,10 @@ def enricher(config):
             results = []
             if doctype == 'Kalibrate':
                 results = enr.enrich_kal_scan(scandoc)
-                message_write_queue.extend(results)
+                message_write_queue.append(results)
             elif doctype == 'SIM808':
                 results = enr.enrich_sim808_scan(scandoc)
-                message_write_queue.extend(results)
+                message_write_queue.append(results)
             elif doctype == 'GPS':
                 results = enr.enrich_gps_scan(scandoc)
                 gps_location = scandoc
