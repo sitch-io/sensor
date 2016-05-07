26cef2be5ab4bb2e3536f86bc420c4ae48c1763b
Ash Wilson <ash.d.wilson@gmail.com>
2016-05-07 02:23:35 -0700

    Logger correction

--- sitch/sitchlib/logger.py
+++ sitch/sitchlib/logger.py
@@ -72,5 +72,22 @@ class LogHandler:
         return
 
     def transmit_log_message(self, log_file_type, message):
-        """You should only ever send a string to this method"""
-        self.ls_logger.info(message)
+        """You should only ever send a dict to this method"""
+        try:
+            self.ls_logger.info(message)
+            return
+        except:
+            print "Unsble to send message.  Retrying in a few seconds..."
+            time.sleep(3)
+        try:
+            self.ls_logger.info(message)
+            return
+        except:
+            print "Failed again, gonna try one more time..."
+            time.sleep(3)
+        try:
+            self.ls_logger.info(message)
+            return
+        except:
+            print sys.exc_info()[0]
+        return
