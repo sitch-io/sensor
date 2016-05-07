e41713c0448bb258d73032b12eb9e48010883dc3
Ash Wilson <ash.d.wilson@gmail.com>
2016-05-06 19:05:59 -0700

    fix in init

--- sitch/sitchlib/__init__.py
+++ sitch/sitchlib/__init__.py
@@ -1,5 +1,5 @@
 from config_helper import ConfigHelper
-from enrigher import Enricher
+from enricher import Enricher
 from logger import LogHandler
 from sim808 import FonaReader
 from utility import Utility
