3afb437ad28e815852740f4b236ab84d27055398
Ash Wilson <ash.d.wilson@gmail.com>
2017-02-05 15:49:09 -0800

    chg: test: Adding test for SIM900 output !minor

--- sitch/tests/unit/test_unit_gsm_modem.py
+++ sitch/tests/unit/test_unit_gsm_modem.py
@@ -8,6 +8,7 @@ fixturepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 sitchlib = imp.load_module(modulename, file, pathname, description)
 sample_12 = '+CENG: 0,"0154,28,00,310,411,12,000f,00,05,178d,255"'
 sample_7 = '+CENG: 1,"0128,15,30,310,411,178d"'
+sample_sim900 = '+CENG:0,"0672,12,99,310,260,80,0007,06,242,113,2"'
 
 
 class TestGsmModem:
@@ -19,6 +20,12 @@ class TestGsmModem:
     def test_process_line_12(self):
         result = sitchlib.GsmModem.process_line(sample_12)
         for k, v in result.items():
+            assert v is not None
+
+    def test_process_sim900(self):
+        result = sitchlib.GsmModem.process_line(sample_sim900)
+        for k, v in result.items():
+            print result
             assert v is not None
 
     def test_process_line_7(self):
