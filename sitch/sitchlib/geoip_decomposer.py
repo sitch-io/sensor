392786c51b5c1db26c0e84839e62bb0bfd37f0a3
Ash Wilson <ash.d.wilson@gmail.com>
2017-02-26 20:47:49 -0800

    chg: dev: Now, adding that thing I referenced in the last commit.  !minor

---
+++ sitch/sitchlib/geoip_decomposer.py
@@ -0,0 +1,17 @@
+class GeoipDecomposer(object):
+    @classmethod
+    def decompose(cls, scan_document):
+        results_set = [("geoip", scan_document)]
+        if not GeoipDecomposer.scan_document_is_valid(scan_document):
+            return []
+        else:
+            return results_set
+
+    @classmethod
+    def scan_document_is_valid(cls, scan_document):
+        is_valid = False
+        if "geometry" in scan_document:
+            if "coordinates" in scan_document["geometry"]:
+                if scan_document["geometry"]["coordinates"] != [0, 0]:
+                    is_valid = True
+        return is_valid
