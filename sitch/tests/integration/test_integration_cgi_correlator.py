b8ddb1f59ccb8719159eb28cb4ec7638b47b6081
Ash Wilson <ash.d.wilson@gmail.com>
2017-03-02 21:49:10 -0800

    chg: dev: Re-aligning CGI correlator tests with accurate scan sample data from GPS decomposer !minor

--- sitch/tests/integration/test_integration_cgi_correlator.py
+++ sitch/tests/integration/test_integration_cgi_correlator.py
@@ -12,14 +12,12 @@ feedpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 file, pathname, description = imp.find_module(modulename, [modulepath])
 sitchlib = imp.load_module(modulename, file, pathname, description)
 
-geo_state = {"gps":
-             {"geometry":
+geo_state = {"geometry":
               {"coordinates":
-                  [-122.431297, 37.773972]}}}
-bad_geo_state = {"gps":
-                 {"geometry":
+                  [-122.431297, 37.773972]}}
+bad_geo_state = {"geometry":
                   {"coordinates":
-                      [0, 0]}}}
+                      [0, 0]}}
 
 states = ["CA"]
 
