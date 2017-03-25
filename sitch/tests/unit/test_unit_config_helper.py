2a40e92b3dd17da1bc6c1e1306f1082b7e05541e
Ash Wilson <ashmastaflash@users.noreply.github.com>
2017-03-25 12:14:53 -0700

    Feature/move feed location (#68)
    chg: usr: Changed feed dir to /data/sitch/feed, and log dir to /data/sitch/log/.  Relocated filebeat registry to /data/sitch/log/fb_registry

--- sitch/tests/unit/test_unit_config_helper.py
+++ sitch/tests/unit/test_unit_config_helper.py
@@ -8,6 +8,20 @@ fixturepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                            "../fixture/ceng.txt")
 sitchlib = imp.load_module(modulename, file, pathname, description)
 
+test_conf = {"filebeat.prospectors": [
+              {"input_type": "log",
+               "document_type": "cells",
+               "keys_under_root": True,
+               "paths": [
+                 "/var/log/sitch/cells.log"
+                ]},
+              {"input_type": "log",
+               "document_type": "scan",
+               "keys_under_root": True,
+               "paths": [
+                 "/var/log/sitch/scanner.log"
+                ]}]}
+
 
 class TestConfigHelper:
     def create_config(self):
@@ -18,6 +32,16 @@ class TestConfigHelper:
         config.mcc_list = []
         config.feed_url_base = "https://s3.amazonaws.com/Sitchey//GSM"
         return config
+
+    def test_unit_set_filebeat_file_paths(self):
+        print test_conf
+        res = sitchlib.ConfigHelper.set_filebeat_logfile_paths("/pre/fix/",
+                                                               test_conf)
+        print res
+        assert len(res["filebeat.prospectors"][0]["paths"]) == 1
+        assert res["filebeat.prospectors"][0]["paths"][0] == "/pre/fix/cells.log"
+        assert len(res["filebeat.prospectors"][1]["paths"]) == 1
+        assert res["filebeat.prospectors"][1]["paths"][0] == "/pre/fix/scanner.log"
 
     def test_unit_create_config_object(self):
         assert self.create_config()
